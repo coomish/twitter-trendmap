13865e761d9ee46d639d6d0f2279e4000d0c35e7
robinhirt <robinhirt@gmail.com>
2016-10-22 21:55:39 +0200

    added chart

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,7 +1,6 @@
 (function() {
 
     angular.module('trendMap')
-
     // inject the Profile service factory into our controller
     .controller('mainCtrl', mainController);
 
@@ -10,6 +9,7 @@
         $scope.analyze = false;
         $scope.showtweets = false;
         $scope.formData = {};
+        $scope.test = 'asd'
         $scope.formData.tweet_count = 50;
         $scope.loading = false;
 
@@ -36,5 +36,21 @@
                 });
             }
         };
+        Highcharts.chart('highchartCont', {
+          title: {
+            text: 'Tweet Data'
+          },
+
+          xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+              'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+            ]
+          },
+
+          series: [{
+            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+          }]
+        });
+
     }
 })();
